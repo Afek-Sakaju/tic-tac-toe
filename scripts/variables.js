2b36f20f8fcddf0c380be3498088ec5063136312
afekTheMiniLearner <afeksacajo@gmail.com>
2023-06-07 18:10:49 +0300

    changed utils to be separated to 2 diff files instead of all in one

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,14 +1,14 @@
-const gameMatrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-].map((row) => row.map((num) => new ActionButton(`actionButton${num}`)));
-
 const soundButton = new SoundButton('sound-button');
 const startGameButton = new GameElement('start-button');
 const winnerPopup = new GameElement('winner-container');
 const winnerText = new GameElement('winner-text');
 const currentTurnDisplay = new GameElement('turn-display');
+
+const gameMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+].map((row) => row.map((num) => new ActionButton(`actionButton${num}`)));
 
 let currentTurn = 'O';
 let isFinishedGame = true;
