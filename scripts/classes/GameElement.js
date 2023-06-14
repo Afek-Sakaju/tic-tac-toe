e831a53d766324eafdb22b0c02e936eab90688ac
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-14 23:09:12 +0300

    split the classes to 2 separated files

--- scripts/classes/GameElement.js
+++ scripts/classes/GameElement.js
@@ -55,18 +55,3 @@ class GameElement {
         this.deleteAttribute(['onclick']);
     }
 }
-
-class ActionButton extends GameElement {
-    constructor(id, modes) {
-        super(id, modes);
-        this._sign = null;
-    }
-
-    set sign(sign) {
-        this._sign = sign;
-    }
-
-    get sign() {
-        return this._sign;
-    }
-}
