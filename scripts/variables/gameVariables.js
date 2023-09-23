584d82964a07dbc9fb1ade1341d649e64f4d9277
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-23 23:41:17 +0300

    improved soome more naming to the new naming convention of board cell instead of board button and some more logic improvement

--- scripts/variables/gameVariables.js
+++ scripts/variables/gameVariables.js
@@ -7,19 +7,7 @@ const gameBoardMatrix = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
-].map((row) =>
-  row.map((num) => {
-    const cellElement = document.getElementById(`actionButton${num}`);
-    cellElement.addEventListener('click', () => {
-      const isDisabled = cellElement.getAttribute('disabled');
-
-      // Subtracting 1 because the matrix calculations are based on index.
-      if (!isDisabled) selectBoardCell(num - 1);
-    });
-
-    return { cellElement, sign: null };
-  })
-);
+].map((row) => row.map((num) => getInitializedBoardCell(num)));
 
 let currentTurn = 'o';
 let isGameFinished = true;
