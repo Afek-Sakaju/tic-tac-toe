dba432c77fa31793cc73c530b49cca24d73b5d9d
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-24 00:24:15 +0300

    improved more logic and functionality of the app especially the face that now the board cells are initialized in the js code instead of hard coded in the html

--- scripts/variables/gameVariables.js
+++ scripts/variables/gameVariables.js
@@ -3,11 +3,7 @@ const startGameButton = document.getElementById('start-btn');
 const currentTurnStatusDisplay = document.getElementById('turn-status-info');
 const toggleMuteButton = document.getElementById('sound-btn');
 
-const gameBoardMatrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-].map((row) => row.map((num) => getInitializedBoardCell(num)));
+const gameBoardMatrix = getInitializedGameBoardMatrix(3, 3);
 
 let currentTurn = 'o';
 let isGameFinished = true;
