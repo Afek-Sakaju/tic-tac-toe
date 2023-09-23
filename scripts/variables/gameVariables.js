656d818c3a4bad9071cfe21f6354e5986e87fbe2
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-23 23:19:23 +0300

    removed the class GameElement because its just makes too much complication in simple code this project is small and theres no need to create so much methods and class with complex implementation to such simple app

--- scripts/variables/gameVariables.js
+++ scripts/variables/gameVariables.js
@@ -1,17 +1,24 @@
 /* eslint-disable prefer-const */
-const startGameButton = new GameElement('start-btn');
-const currentTurnStatusDisplay = new GameElement('turn-status-info');
-const toggleMuteButton = new GameElement('sound-btn');
+const startGameButton = document.getElementById('start-btn');
+const currentTurnStatusDisplay = document.getElementById('turn-status-info');
+const toggleMuteButton = document.getElementById('sound-btn');
 
 const gameBoardMatrix = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ].map((row) =>
-  row.map((num) => ({
-    cellElement: document.getElementById(`actionButton${num}`),
-    sign: null,
-  }))
+  row.map((num) => {
+    const cellElement = document.getElementById(`actionButton${num}`);
+    cellElement.addEventListener('click', () => {
+      const isDisabled = cellElement.getAttribute('disabled');
+
+      // Subtracting 1 because the matrix calculations are based on index.
+      if (!isDisabled) selectBoardCell(num - 1);
+    });
+
+    return { cellElement, sign: null };
+  })
 );
 
 let currentTurn = 'o';
