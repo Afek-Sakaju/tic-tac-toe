a5e8584ac1fb8e101ee49a6423557158a8e0df57
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-10-02 14:42:48 +0300

    reordered the elements file and added there gaps

--- scripts/utils/variables/elements.js
+++ scripts/utils/variables/elements.js
@@ -1,9 +1,11 @@
+const gameBoardMatrix = getInitializedGameBoardMatrix();
+
 const toggleMuteButton = document.getElementById(ELEMENTS_IDS.SOUND_BUTTON);
 
 const playerScoreDisplay = document.getElementById(ELEMENTS_IDS.PLAYER_SCORE);
+
 const tieScoreDisplay = document.getElementById(ELEMENTS_IDS.TIE_SCORE);
+
 const computerScoreDisplay = document.getElementById(
   ELEMENTS_IDS.COMPUTER_SCORE
 );
-
-const gameBoardMatrix = getInitializedGameBoardMatrix();
